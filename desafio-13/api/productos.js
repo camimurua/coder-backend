4c9846dd2ec9c2bedd6c891216bb0fdb4d6e449f
Cami Murua <camilamurua99@gmail.com>
2021-06-22 22:49:27 -0300

    entrega de desafio 13

---
+++ desafio-13/api/productos.js
@@ -0,0 +1,33 @@
+class Productos {
+    constructor() {
+        this.productos = []
+        this.id = 0
+    }
+
+    listar() {
+        return this.productos;
+    }
+
+    listarAll() {
+        return this.productos.length? this.productos : {error : 'no hay productos cargados'}
+    }
+
+    guardar(prod) {
+        prod.id = ++this.id
+        this.productos.push(prod)
+    }
+
+    actualizar(prod,id) {
+        prod.id = Number(id)
+        let index = this.productos.findIndex( prod => prod.id == id)
+        this.productos.splice(index,1,prod)
+    }
+
+    borrar(id) {
+        let index = this.productos.findIndex( prod => prod.id == id)
+        return this.productos.splice(index,1)
+    }
+
+}
+
+export default Productos
